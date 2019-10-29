490271a40dd128726b92579d5b2f46bf431d9bb0
bricezakra <bricezakra@live.com>
2019-10-29 08:14:03 -0500

    server.js file set up

--- server.js
+++ server.js
@@ -15,3 +15,61 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+
+// Initialize Express for debugging & body parsing
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// Serve Static Content
+app.use(express.static(process.cwd() + '/public'));
+
+// Express-Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// Database Configuration with Mongoose
+// ---------------------------------------------------------------------------------------------------------------
+// Connect to localhost if not a production environment
+if (process.env.NODE_ENV == 'production') {
+    mongoose.connect('');
+} else {
+    mongoose.connect('');
+    // YOU CAN IGNORE THE CONNECTION URL BELOW THAT WAS JUST FOR DELETING STUFF ON A RE-DEPLOYMENT
+    //mongoose.connect('');
+}
+var db = mongoose.connection;
+
+// Show any Mongoose errors
+db.on('error', function(err) {
+    console.log('Mongoose Error: ', err);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+    console.log('Mongoose connection successful.');
+});
+
+// Import the Comment and Article models
+var Comment = require('./models/comment.js');
+var Article = require('./models/article.js');
+// ---------------------------------------------------------------------------------------------------------------
+
+// DROP DATABASE (FOR MY PERSONAL REFERENCE ONLY - YOU CAN IGNORE)
+// Article.remove({}, function(err) { 
+//    console.log('collection removed') 
+// });
+
+// Import Routes/Controller
+var router = require('./controllers/controller.js');
+app.use('/', router);
+
+
+// Launch App
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Running on port: ' + port);
+});
